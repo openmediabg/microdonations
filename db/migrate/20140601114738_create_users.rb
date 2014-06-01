439a792ab52869c87bfdb911317a71344edddd30
Dimitar Dimitrov <me@ddimitrov.name>
2014-06-01 15:04:33 +0300

    Add a User model with devise

---
+++ db/migrate/20140601114738_create_users.rb
@@ -0,0 +1,38 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :fisrt_name, null: false
+      t.string :last_name,  null: false
+      t.string :email,      null: false
+
+      ## Database authenticatable
+      t.string :encrypted_password, null: false
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+
+      t.timestamps
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
+  end
+end
