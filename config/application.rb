6259548f8809aab418a9db9e350e50dca8a62dbc
Dimitar Dimitrov <me@ddimitrov.name>
2014-06-14 18:21:58 +0300

    Set a default host for sending links in emails

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Microdonations
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.site_domain = 'microdonations.openmedia.bg'
+
     config.generators do |g|
       g.factory_girl false
     end
