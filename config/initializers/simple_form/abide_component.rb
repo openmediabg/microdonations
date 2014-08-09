72a1d30dab4820f36d571c19a1dbbe11ab3403ee
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-09 13:45:32 +0300

    Use the same error messages on server & client

--- config/initializers/simple_form/abide_component.rb
+++ config/initializers/simple_form/abide_component.rb
@@ -2,15 +2,31 @@ module SimpleForm
   module Components
     module Abides
       def abide_attributes
-        if /^(?<field_to_confirm>\w+)_confirmation$/ =~ attribute_name.to_s and
-            object.class.validators_on(field_to_confirm).any? { |v| v.kind == :confirmation }
+        if field_to_confirm
           input_html_options['data-equalto'] = "#{object_name}_#{field_to_confirm}"
         end
         nil
       end
 
       def abide_message
-        options[:abide] || translate(:abides)
+        options[:abide] || translate(:abides) || infer_validation_message
+      end
+
+      private
+
+      def infer_validation_message
+        if field_to_confirm
+          object.errors.generate_message(attribute_name, :confirmation)
+        elsif has_required?
+          object.errors.generate_message(attribute_name, :required)
+        end
+      end
+
+      def field_to_confirm
+        if /^(?<field_to_confirm>\w+)_confirmation$/ =~ attribute_name.to_s and
+            object.class.validators_on(field_to_confirm).any? { |v| v.kind == :confirmation }
+          field_to_confirm
+        end
       end
     end
   end
