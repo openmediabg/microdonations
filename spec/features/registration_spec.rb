f3a3d51494ebb829405e5f352207b041b04952a6
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-09 14:59:37 +0300

    A spec for registration with invalid details

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -12,4 +12,17 @@ feature 'Registration' do
 
     page.should have_content 'Добре дошли! Вие се регистрирахте успешно.'
   end
+
+  scenario 'Attempting registration with invalid details' do
+    visit sign_up_path
+    fill_in 'Вашият имейл',    with: 'ivan@example.org'
+    fill_in 'Парола',          with: 'something'
+    fill_in 'Паролата отново', with: 'another'
+    fill_in 'Малко име',       with: 'Иван'
+    fill_in 'Фамилия',         with: 'Петров'
+    click_on 'Регистрация'
+
+    expect(current_path).to eq sign_up_path
+    expect(User.count).to eq 0
+  end
 end
